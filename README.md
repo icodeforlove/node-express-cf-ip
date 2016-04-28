4f47b9acb67fee3f3127da1d8561de3dbe6a0204
Chad Scira <chadvscira@gmail.com>
2016-04-28 15:45:52 +0700

    added some security

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-adds back the original users IP, and sets the cloudflare IP as `forwarder_ip`, this will fix logging software because it update the original ip property
+Adds back the original users IP, and sets the cloudflare IP as `forwarder_ip`, this will fix logging software because it update the original ip property. It will only make IP modifications if they come from OFFICIAL cloudflare servers.
 
 # usage
 app.use(require('express-cf-ip'));
--- index.js
+++ index.js
@@ -1,5 +1,73 @@
-module.exports = function (request, response, next) {
-    if (request.headers['cf-connecting-ip'] || request.headers['x-forwarded-for']) {
+var request = require('request'),
+    ipRange = require('ip-range-check'),
+    cloudflareIpRanges = [],
+    cloudflareIpRangesUpdatedAt = null,
+    cloudflareIpRangesUpdating = false,
+    queuedRequests = [],
+    debug = require('debug')('express-cf-ip');
+
+function updateCloudflareRanges () {
+    var newRanges = [];
+
+    if (cloudflareIpRangesUpdating) {
+        return;
+    }
+
+    cloudflareIpRangesUpdating = true;
+
+    debug('getting IPV4 ranges from cloudflare');
+
+    request.get('https://www.cloudflare.com/ips-v4', function (error, response) {
+        if (error) {
+            throw new Error('express-cf-ip:' + error.message);
+        }
+
+        newRanges = newRanges.concat(response.body.trim().split('\n'));
+
+        debug('getting IPV6 ranges from cloudflare');
+
+        request.get('https://www.cloudflare.com/ips-v6', function (error, response) {
+            if (error) {
+                throw new Error('express-cf-ip:' + error.message);
+            }
+
+            newRanges = newRanges.concat(response.body.trim().split('\n'));
+
+            cloudflareIpRangesUpdating = false;
+            cloudflareIpRanges = newRanges;
+            cloudflareIpRangesUpdatedAt = new Date();
+
+            // clear queue
+            if (queuedRequests.length) {
+                debug('draining request queue of ', queuedRequests.length);
+
+                queuedRequests.forEach(function (queuedRequest) {
+                    queuedRequest();
+                });
+                queuedRequests = [];
+            }
+        });
+    });
+}
+
+// update ip list on boot
+updateCloudflareRanges();
+
+module.exports = function expressCfIp(request, response, next) {
+    // update daily
+    if (cloudflareIpRangesUpdatedAt && (new Date() - cloudflareIpRangesUpdatedAt) > 60 * 60 * 24 * 1000) {
+        updateCloudflareRanges();
+    }
+
+    // queue requests until cloudflares IP's have been set
+    if (!cloudflareIpRangesUpdatedAt) {
+        return queuedRequests.push(function () {
+            expressCfIp(request, response, next);
+        });
+    }
+
+    // swap IP if cloudflare set it
+    if (ipRange(request.ip, cloudflareIpRanges) && (request.headers['cf-connecting-ip'] || request.headers['x-forwarded-for'])) {
         Object.defineProperty(request, 'forwarder_ip', {
             configurable: true,
             enumerable: true,
